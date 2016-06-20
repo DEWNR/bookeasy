5fd9e8adf738a1be2b567b46a18a17d9c588dbf7
Ben Kannenberg <bkannenberg@gmail.com>
2016-06-20 16:44:19 +0930

    refactor statewide map

---
+++ src/javascripts/parks-data.js
@@ -0,0 +1,184 @@
+var productsData = {
+    "All" :{
+        "Things to book": {
+            "Camping / Accommodation": false,
+            "Vehicle Entry Fee": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "1",
+        "url": "http://www.environment.sa.gov.au/parks/development/Booking#All"
+    },
+    "Beachport Conservation Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "2",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/beachport-conservation-park"
+    },
+    "Belair National Park" :{
+        "Things to book": {
+            "Camping / Accommodation": false,
+            "Vehicle Entry": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": true,
+            "Facilities": true,
+            "School Bookings": false
+        },
+        "index": "3",
+        "url": "http://www.environment.sa.gov.au/parks/development/Booking/belair-national-park"
+    },
+    "Bool Lagoon Game Reserve" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": true,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "4",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/Bool_Lagoon_Hacks_Lagoon"
+    },
+    "Canunda National Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "5",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/canunda-national-park"
+    },
+    "Coorong National Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "6",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/Coorong_National_Park"
+    },
+    "Deep Creek Conservation Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": true,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": true
+        },
+        "index": "7",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Fleurieu_Peninsula/deep-creek-conservation-park"
+    },
+    "Dutchmans Stern Conservation Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "8",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/flinders-ranges-outback/The_Dutchmans_Stern_Conservation_Park"
+    },
+    "Flinders Chase National Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": true,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "9",
+        "url": "http://www.environment.sa.gov.au/parks/development/Booking/Kangaroo-Island"
+    },
+    "Innes National Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": true,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": true
+        },
+        "index": "10",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Yorke_Peninsula/innes-national-park"
+    },
+    "Little Dip Conservation Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "11",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/little-dip-conservation-park"
+    },
+    "Mount Remarkable National Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": true,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": true
+        },
+        "index": "12",
+        "url": "http://www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/flinders-ranges-outback/mount-remarkable-national-park"
+    },
+    "Tallaringa Conservation Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": true,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "13",
+        "url": "http://www.environment.sa.gov.au/parks/development/Booking/tallaringa-conservation-park"
+    },
+    "Vulkathunha-Gammon ranges National Park" :{
+        "Things to book": {
+            "Camping / Accommodation": true,
+            "Vehicle Entry": false,
+            "Diving": false,
+            "Snorkelling": false,
+            "Tennis Courts": false,
+            "Facilities": false,
+            "School Bookings": false
+        },
+        "index": "14",
+        "url": "http://www.environment.sa.gov.au/parks/development/Booking/vulkathunha-gammon_ranges_NP"
+    }
+};
