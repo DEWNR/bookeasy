eb13aba0e8a10a40466f3346ff0c1e03062a8a01
Ben Kannenberg <bkannenberg@gmail.com>
2016-10-25 15:21:23 +1030

    change headings

--- src/javascripts/inline/details-gadget.js
+++ src/javascripts/inline/details-gadget.js
@@ -39,7 +39,26 @@ function getOperatorData(id) {
     })
     .done(function(data) {
 
+        var backURL = '#';
+
         if (data.length) {
+
+            console.log(data[0]);
+
+            backURL = '/#' + data[0].Location;
+
+            if(data[0].IsTourManager == true) {
+
+                // add location title
+                $('#content').prepend('<h1>' + data[0].Location + '</h1>');
+
+            } else {
+                // add location title
+                $('#content').prepend('<h1>' + data[0].Location + '</h1><p class="">' + data[0].TradingName + '</p>');
+            }
+
+            // add back button
+            $('#bookeasy__details-gadget').before('<div class="button-list"><a class="button-list__button button-list__button--back" href="' + backURL + '">Back</a></div>');
 
             // insert t&c information
             $('#bookeasy__conditions').html('<h3>Terms and conditions</h3>' + text2HTML(data[0].CancellationPolicy));
