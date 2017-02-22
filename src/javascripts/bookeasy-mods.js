f5698acc3f8f3540af54eae6a118d728b6a77575
Ben Kannenberg <bkannenberg@gmail.com>
2017-02-22 14:05:17 +1030

    region gadget manipulations

--- src/javascripts/bookeasy-mods.js
+++ src/javascripts/bookeasy-mods.js
@@ -6,82 +6,82 @@ var selectedDays = 1;
 
 $(document).on('gadget.script.loaded', function() {
 
-    // watch gadget.js 'grid.rendered', do this when fired.
+    // initialise region gadget watchers
+    IMUtility.pushRegionGadgetLoadedEvent();
+    IMUtility.pushRegionGadgetChangedEvent();
+
+    // check if mobile device
+    isMobile = windowWidth < 767 ? true : false;
+
+    // check if type is accomodation
+    if($('html').hasClass('is-accom')) {
+        isAccom = true;
+    }
+
+
+
+
+
+    // style tabs once refine tools are built
+    $w.event.subscribe('region.refinetools.built', function() {
+
+        if (($('.tabs-group').size() > 0) && ($('.gadget__region-tabs').size() > 0)) {
+            $('.gadget__region-tabs').remove();
+        }
+
+        $('.tabs-group').addClass('gadget__region-tabs').removeClass('tabs-group');
+
+    });
+
+
+
+
+
+    // region.gadget.built is fired when using the region-gadget
+    $w.event.subscribe('region.gadget.built', function() {
+
+        // detect when last row loaded
+        $('.im-grid .accom tbody>tr:last-child .name').IMElementExists(function() {
+
+            $('.prices-grid td.date').addClass('hidden-xs'); // used in bookeasy-utility to determin if loaded
+
+            // get days selected and add/remove class
+            selectedDays = getDaysSelected();
+
+            if(isMobile) {
+
+                updateProductRows('region');
+
+            }
+
+            // load hi-res images
+            insertImages('region');
+
+        });
+
+    });
+
+
+
+
+
+    // grid.rendered is fired when using the details-gadget
     $w.event.subscribe('grid.rendered', function() {
 
-        // should only be in here while viewing details-gadget
-
-        // check if mobile device
-        isMobile = windowWidth < 767 ? true : false;
-
-        // check if type is accomodation
-        if($('html').hasClass('is-accom')) {
-            isAccom = true;
-        }
-
-        // get days selected
+        // get days selected and add/remove class
         selectedDays = getDaysSelected();
-
-        // change things up if lots of days selected
-        if(selectedDays > 5) {
-            $('html').addClass('manyCols');
-        } else {
-            $('html').removeClass('manyCols');
-        }
 
         // detect when last row loaded
         $('.im-grid tbody>tr:last-child .OperatorInfo').IMElementExists(function() {
 
             if(isMobile) {
 
-                var $dateHeaders = $('.im-grid thead td.date').clone();
-
-                // loop over each product row and modify
-                $('.im-grid tr.odd, .im-grid tr.even').each(function() {
-                    var $product = $(this);
-                    var priceTable = '<td><table class="price_table">';
-
-                    // create price table
-                    $product.find('td.price').each(function(index) {
-                        priceTable += '<tr><th>' + $dateHeaders[index].innerHTML + '</th><td class="price_table__price">' + $(this).html() + '</td></tr>';
-                    });
-
-                    priceTable += '</td></table>'
-
-                    // move thumbnail image
-                    $product.find('div.thumb').prependTo($product).wrap('<td />');
-
-                    // add price table
-                    $product.append(priceTable);
-
-                    // wrap each td in a row
-                    $product.find('td').wrap('<tr class="product__row" />');
-
-                    // insert new product table
-                    $product.wrapInner('<td><table class="product"></td>')
-
-                    // remove quantity (not needed for accomodation)
-                    $('td.quantity').remove();
-
-                    // remove quantity (not needed for accomodation)
-                    $('td.total').parent().addClass('product__row--total');
-
-                    // add specials
-                    $product.find('.product__row--total').after('<tr class="product__row  product__row--specials"><td></td></tr>');
-                    $product.find('.product__row--specials td').append($product.find('div.specials'));
-
-                });
-
-                // remove header content
-                $('thead','.im-grid').remove();
-
-                // remove price columns
-                $('td.price').remove();
+                updateProductRows('details');
 
             }
 
             // load hi-res images
-            insertImages();
+            insertImages('details');
 
         });
 
@@ -89,6 +89,62 @@ $(document).on('gadget.script.loaded', function() {
     });
 
 });
+
+function updateProductRows(gadget) {
+    var $dateHeaders = $('.im-grid thead td.date').clone();
+    var sSelector = '.im-grid tr.odd, .im-grid tr.even';
+
+    if(gadget === 'region') {
+        sSelector = '.im-grid .accom tr.odd, .im-grid .accom tr.even';
+    }
+
+    // loop over each product row and modify
+    $(sSelector).each(function() {
+        var $product = $(this);
+        var priceTable = '<td><table class="price_table">';
+
+        // create price table
+        $product.find('td.price').each(function(index) {
+            priceTable += '<tr><th>' + $dateHeaders[index].innerHTML + '</th><td class="price_table__price">' + $(this).html() + '</td></tr>';
+        });
+
+        priceTable += '</td></table>'
+
+        // move thumbnail image
+        $product.find('div.thumb').prependTo($product).wrap('<td />');
+
+        // add price table
+        $product.append(priceTable);
+
+        // wrap each td in a row
+        $product.find('td').wrap('<tr class="product__row" />');
+
+
+        if(gadget === 'region') {
+            $('.inline-header').remove();
+        }
+
+        // insert new product table
+        $product.wrapInner('<td><table class="product" width="100%"></td>');
+
+        // remove quantity (not needed for accomodation)
+        $('td.quantity').remove();
+
+        // remove quantity (not needed for accomodation)
+        $('td.total').parent().addClass('product__row--total');
+
+        // add specials
+        $product.find('.product__row--total').after('<tr class="product__row  product__row--specials"><td></td></tr>');
+        $product.find('.product__row--specials td').append($product.find('div.specials'));
+
+    });
+
+    // remove header content
+    $('thead','.im-grid').remove();
+
+    // remove price columns
+    $('td.price').remove();
+}
 
 
 
@@ -98,10 +154,43 @@ function getDaysSelected() {
     var sReturn = 1;
 
     if (typeof variable === 'undefined') {
-        sReturn = $('thead>tr>td.date').length;
+        sReturn = $('thead>tr>td.date', '.accom').length;
+    }
+
+    // change things up if lots of days selected
+    if(sReturn > 5) {
+        $('html').addClass('manyCols');
+    } else {
+        $('html').removeClass('manyCols');
     }
 
     return sReturn
+}
+
+
+
+
+
+function getMapData() {
+    var oMaps = [];
+
+    $.each(campgroundData, function(key, value) {
+
+        if ( key.indexOf(',') != -1 ){
+
+            var aIDs = key.split(',');
+
+            $.each(aIDs, function(i) {
+                oMaps[aIDs[i]] = value;
+            });
+
+        } else {
+            oMaps[key] = value;
+        }
+
+    });
+
+    return oMaps;
 }
 
 
@@ -115,46 +204,20 @@ function insertImages(gadget) {
     $('img', '.thumb').each(function(){
         var $thumbnail = $(this);
         var imagePath = $thumbnail.attr('rel');
-        var productTitle = $thumbnail.parent().prev().text();
+        var productTitle = '';
 
-        $thumbnail.attr('src', imagePath).wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + productTitle + '"></a>');
+
+        if(gadget == 'details') {
+            productTitle = $thumbnail.parent().prev().text();
+            $thumbnail.attr('src', imagePath).wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + productTitle + '"></a>');
+        } else {
+            imagePath = imagePath.replace('thumbs/461', 'images');
+            productTitle = $thumbnail.parent().siblings('.name').text();
+            $thumbnail.wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + productTitle + '"></a>');
+        }
+
     });
 
-    // $('.im-grid tbody tr').each(function() {
-    //
-    //     if ($(this).find('td.name div.thumb').size()) {
-    //
-    //         // Move thumb to its own column & add fancybox
-    //         $thumbImage = $(this).find('.thumb > img');
-    //         imagePath = $thumbImage.attr('rel');
-    //
-    //         thumbCount = thumbCount + 1;
-    //
-    //         $thumbImage.attr('src', imagePath).wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + $(this).find('a:not([class])').text() + '"></a>');
-    //
-    //
-    //     } else if ($(this).find('div.thumb').size()) {
-    //
-    //         // Move thumb to its own column & add fancybox
-    //         $thumbImage = $(this).find('.thumb > img');
-    //         imagePath = $thumbImage.attr('rel').replace('thumbs/461', 'images');
-    //
-    //         thumbCount = thumbCount + 1;
-    //
-    //         $(this).find('div.thumb').append('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + $(this).find('a.name').text() + '"><img src="' + imagePath + '" /></a>');
-    //         $(this).find('div.thumb > img').hide();
-    //
-    //     } else {
-    //         $(this).addClass('no-image');
-    //     }
-    //
-    // });
-    //
-    // if (thumbCount == 0) {
-    //     // remove header thumb
-    //     $('.im-grid thead tr td.thumb').remove();
-    //     $('.im-grid tbody tr div.thumb').remove();
-    // }
 }
 
 
