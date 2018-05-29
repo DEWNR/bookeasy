1a9fab76a4235569ed26b79825376d27de889bd8
Nick <>
2018-05-29 11:57:35 +0930

    added variable to enable/disable bookeasy functionality

--- src/javascripts/booking-statewide.js
+++ src/javascripts/booking-statewide.js
@@ -7,6 +7,11 @@
 // BE['gadget']['region']['text']['viewMap'] = 'Map';
 // BE['gadget']['search']['text']['adults']['label'] = 'Adults';
 // var t0 = window.performance.now();  // get precise time to compare against later
+// var bookingsDisabled = true;  //set to true disable bookings
+if (!bookingsDisabled) {  //if undefined
+    var bookingsDisabled = false;       // define it
+}
+
 var isRegionGadgetPage = false;
 var dataExists = false;
 var productsData;
@@ -17,6 +22,7 @@ var hideProductTypes = [];
 var bShowRegionGadget = 1;
 var windowURL = window.location.href;
 var dataURL = '//www.environment.sa.gov.au/feed.rss?listname=npsa-cl-products-data';
+var initialIs;
 
 
 if ( windowURL.search(/localhost/i) >= 0 || windowURL.search(/127.0.0.1/i) >= 0 ) {
@@ -62,6 +68,14 @@ function dataCleanAndRun() {
         hideProductTypes = ['tours','carhire','events','packages'];
 
         urlHash = urlHash.replace(/%20/g, ' ');
+
+
+        initialIs = true;
+        // console.log('initial urlHash: ' + urlHash);
+        if(window.ga && ga.create) {
+            ga('send', 'event', 'Booking statewide', 'initial filter', urlHash);
+        }
+
 
         // fix for Nullarbor
         if(urlHash === 'Nullarbor National Park, Wilderness Protection Area and Regional Reserve') {
@@ -194,28 +208,48 @@ function displayProductsData() {
 
     //productsData[urlHash] is better than using eval to turn string into object name
     try {
-        $.each(productsData[urlHash]["Things to book"], function(key, val) {
-            if (key == 'Vehicle Entry Fee') {
-                key = 'Vehicle Entry Fees'
-            }
+        if (bookingsDisabled == false) {
 
-            //we don't need to display camping button if we are already looking at camping
-            if (key !== 'Camping / Accommodation' && val === true) {
-                //Check urls
-                bookingURL = productsData[urlHash].url + '/' + (key.replace(/ /g , '-')).toLowerCase();
-
-                if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-                    bookingURL += '.html';
+            $.each(productsData[urlHash]["Things to book"], function(key, val) {
+                if (key == 'Vehicle Entry Fee') {
+                    key = 'Vehicle Entry Fees'
                 }
 
-                $('.button-list').append($('<a href="'+ bookingURL + '"><span>' +key+ '</span></a>').addClass('button-list__button '+key).attr('data', key));
-                $('.'+key).click(  function(){ typeShow('tours'); }  );
-            }
+                //we don't need to display camping button if we are already looking at camping
+                if (key !== 'Camping / Accommodation' && val === true) {
+                    //Check urls
+                    bookingURL = productsData[urlHash].url + '/' + (key.replace(/ /g , '-')).toLowerCase();
 
-            if (key == 'Camping / Accommodation' && val === true) {
-                hasAccomodation = true;
-            }
-        });
+                    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+                        bookingURL += '.html';
+                    }
+
+                    $('.button-list').append($('<a href="'+ bookingURL + '"><span>' +key+ '</span></a>').addClass('button-list__button '+key).attr('data', key));
+                    $('.'+key).click(  function(){ typeShow('tours'); }  );
+                }
+
+                if (key == 'Camping / Accommodation' && val === true) {
+                    hasAccomodation = true;
+                }
+            });
+
+        } else {
+            //still show school bookings if bookingsDisabled
+            $.each(productsData[urlHash]["Things to book"], function(key, val) {
+                if (key == 'School Bookings') {
+                    //Check urls
+                    bookingURL = productsData[urlHash].url + '/' + (key.replace(/ /g , '-')).toLowerCase();
+
+                    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+                        bookingURL += '.html';
+                    }
+
+                    $('.button-list').append($('<a href="'+ bookingURL + '"><span>' +key+ '</span></a>').addClass('button-list__button '+key).attr('data', key));
+                }
+            });
+
+        }
+
 
     } catch(error) {
         console.error('ERROR: Problem reading json data. '+error);
@@ -247,6 +281,18 @@ function bookeasy() {
 
     cleanGoogleMaps();
 
+    if (initialIs == false) {
+        // console.log('url hash: ' + urlHash);
+        if(window.ga && ga.create) {
+            ga('send', 'event', 'Booking statewide', 'filter chosen', urlHash );
+        }
+        initialIs = true;
+    } else {
+        // console.log('change initial to false');
+        initialIs = false;
+    }
+
+
     $('.booking__early-text').html('');  //clean old messages
 
     var bookingDate = new Date();
@@ -262,6 +308,7 @@ function bookeasy() {
 
     // only load gadget for those with camping / accomodation
     if (hasAccomodation) {
+
         BE.gadget.region('#bookeasy__region-gadget', {
             accomOnlyMode: true, // only display accommodation
             adults: 1,
@@ -287,15 +334,26 @@ function bookeasy() {
             // showRefineTools: false,  //defaultSort wont' work if false!
             vcID: 188
         });
+
     } else {
         // empty container
-        $('#bookeasy__region-gadget').html('<p>Camping and accommodation is not available in this park.</p>');
-        if (urlHash && (urlHash.indexOf('Piccaninnie') !== -1)) {
-            $('.booking__early-text').html('');
-            var $newDiv = $( '<div class="booking__early-text"><p>You will need to enter an indemnity form receipt number at the time of booking. Diving and snorkelling equipment and wetsuits are not provided, you will need to either bring your own equipment/wetsuits or hire it.</p><p>Fees apply to these self-guided activities.</p><p>Please fill out a <a class="link" style="text-decoration: underline;" href="//www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/piccaninnie-ponds-conservation-park/booking/diving/diving-indemnity-form" target="_blank">diving indemnity form</a> or a <a class="link" style="text-decoration: underline;" href="//www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/piccaninnie-ponds-conservation-park/booking/snorkelling/snorkelling-indemnity-form" target="_blank">snorkelling indemnity form</a> for each person diving/snorkelling prior to making the booking.</p><p>Camping and accommodation is not available in this park.</p></div>' );
-            $('.location-selector').after($newDiv);
-            $('#bookeasy__region-gadget').html('');
+
+        if (bookingsDisabled == true) {
+
+            $('#bookeasy__region-gadget').html('<h3>Our bookings system is currently unavailable. Please check back later. <br>Sorry for the inconvenience.</h3>');
+
+        } else {
+
+            $('#bookeasy__region-gadget').html('<p>Camping and accommodation is not available in this park.</p>');
+            if (urlHash && (urlHash.indexOf('Piccaninnie') !== -1)) {
+                $('.booking__early-text').html('');
+                var $newDiv = $( '<div class="booking__early-text"><p>You will need to enter an indemnity form receipt number at the time of booking. Diving and snorkelling equipment and wetsuits are not provided, you will need to either bring your own equipment/wetsuits or hire it.</p><p>Fees apply to these self-guided activities.</p><p>Please fill out a <a class="link" style="text-decoration: underline;" href="//www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/piccaninnie-ponds-conservation-park/booking/diving/diving-indemnity-form" target="_blank">diving indemnity form</a> or a <a class="link" style="text-decoration: underline;" href="//www.environment.sa.gov.au/parks/Find_a_Park/Browse_by_region/Limestone_Coast/piccaninnie-ponds-conservation-park/booking/snorkelling/snorkelling-indemnity-form" target="_blank">snorkelling indemnity form</a> for each person diving/snorkelling prior to making the booking.</p><p>Camping and accommodation is not available in this park.</p></div>' );
+                $('.location-selector').after($newDiv);
+                $('#bookeasy__region-gadget').html('');
+            }
+
         }
+
     }
     // var t1 = window.performance.now();
     // console.log( 'time taken for booking-statewide.js: ' + (t1 - t0) );
