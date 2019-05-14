aa9a80ddc2058ae2fec1bdaa3f41ccc723b1bf6d
Nick <>
2019-05-14 17:36:24 +0930

    display an important notice on Para Wirra vehicle entry for POTM for May.

--- src/javascripts/inline/details-gadget.js
+++ src/javascripts/inline/details-gadget.js
@@ -146,8 +146,31 @@ function getOperatorData(id) {
             //     $('.location-header').append('<div class="location-description"><p>' + data[0].Description + '</p></div>');
             // }
 
+            var importantnotice = '';
+
+            // Display notice 1/2 start
+            if ( operatorID === '90927') {  //if Para Wirra (POTM) then display this notice
+                importantnotice = '<div style="width: 70%;" class="park-alerts__detail  park-alerts__detail--active"><button type="button" class="park-alerts__close js-click-close-notice"></button><h2 class="color--warning">Notice!</h2><p>Para Wirra Conservation Park is our Park of the Month for May. To celebrate we are offering FREE vehicle entry for the entire month of May. If you are visiting during May you do not need to purchase a vehicle entry permit to enter the park.<br><a href="https://www.parks.sa.gov.au/booking#Para%20Wirra%20Conservation%20Park">Back&nbsp;&gt;</a></p></div>'
+                //importantnotice = '<p class="park-alerts__box">Para Wirra Conservation Park is our Park of the Month for May. To celebrate we are offering FREE vehicle entry for the entire month of May. If you are visiting during May you do not need to purchase a vehicle entry permit to enter the park.<p>';
+            }
+            // Display notice 1/2 end
+
             // add back button
-            $('#bookeasy__details-gadget').before('<div class="button-list"><a class="button-list__button button-list__button--back" href="' + backURL + '">Back</a></div>');
+            $('#bookeasy__details-gadget').before('<div class="button-list"><a class="button-list__button button-list__button--back" href="' + backURL + '">Back</a></div>' + importantnotice);
+
+            // Display notice 2/2 start
+            if ( operatorID === '90927') {  //if Para Wirra (POTM) enable close function
+                console.log('here1');
+                $('.js-click-close-notice').on('click', function(e) {
+                    console.log('here1');
+                    $(this).parent().removeClass('park-alerts__detail--active');
+
+                    e.preventDefault();
+
+                    return false;
+                });
+            }
+            // Display notice 2/2 end
 
             // insert t&c information
             $('#bookeasy__conditions').html('<h3>Terms and conditions</h3>' + text2HTML(data[0].CancellationPolicy));
