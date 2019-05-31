25ceb131c94250db434f195e47d537cd54253670
Nick <>
2019-05-31 17:31:28 +0930

    remove Para Wirra POTM notice as it's now passed

--- src/javascripts/inline/details-gadget.js
+++ src/javascripts/inline/details-gadget.js
@@ -150,8 +150,8 @@ function getOperatorData(id) {
 
             // Display notice 1/2 start
             if ( operatorID === '90927') {  //if Para Wirra (POTM) then display this notice
-                importantnotice = '<div style="width: 70%;" class="park-alerts__detail  park-alerts__detail--active"><button type="button" class="park-alerts__close js-click-close-notice"></button><h2 class="color--warning">Notice!</h2><p>Para Wirra Conservation Park is our Park of the Month for May. To celebrate we are offering FREE vehicle entry for the entire month of May. If you are visiting during May you do not need to purchase a vehicle entry permit to enter the park.<br><a href="https://www.parks.sa.gov.au/booking#Para%20Wirra%20Conservation%20Park">Back&nbsp;&gt;</a></p></div>'
-                //importantnotice = '<p class="park-alerts__box">Para Wirra Conservation Park is our Park of the Month for May. To celebrate we are offering FREE vehicle entry for the entire month of May. If you are visiting during May you do not need to purchase a vehicle entry permit to enter the park.<p>';
+                // importantnotice = '<div style="width: 70%;" class="park-alerts__detail  park-alerts__detail--active"><button type="button" class="park-alerts__close js-click-close-notice"></button><h2 class="color--warning">Notice!</h2><p>Para Wirra Conservation Park is our Park of the Month for May. To celebrate we are offering FREE vehicle entry for the entire month of May. If you are visiting during May you do not need to purchase a vehicle entry permit to enter the park.<br><a href="https://www.parks.sa.gov.au/booking#Para%20Wirra%20Conservation%20Park">Back&nbsp;&gt;</a></p></div>'
+                // importantnotice = '<p class="park-alerts__box">Para Wirra Conservation Park is our Park of the Month for May. To celebrate we are offering FREE vehicle entry for the entire month of May. If you are visiting during May you do not need to purchase a vehicle entry permit to enter the park.<p>';
             }
             // Display notice 1/2 end
 
